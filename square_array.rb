d77e50764d60587e16c7cee8216657d51c6eca1b
Matthias Rudin <matthias.rudin@live.com>
2018-11-21 04:25:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each {|element| squared_array << element*element}
+  squared_array
 end
+
+#with map
+#def square_array(array)
+#  array.map {|element| element*element}
+#end
