c1a9699ef7364c8e32bde255bd490f5aabd63670
Matthias Rudin <matthias.rudin@live.com>
2018-11-21 04:25:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
-#def square_array(array)
-#  squared_array = []
-#  array.each {|element| squared_array << element*element}
-#  squared_array
-#end
+def square_array(array)
+  squared_array = []
+  array.each {|element| squared_array << element*element}
+  squared_array
+end
 
+#with map
 def square_array(array)
   array.map {|element| element*element}
 end
